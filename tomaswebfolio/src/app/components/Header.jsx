9de9332d526a84a9a49e41c8868f92c4578e1df0
TomasFerreira66 <tomasff2002@gmail.com>
2024-09-25 07:32:07 +0100

    meteors

--- tomaswebfolio/src/app/components/Header.jsx
+++ tomaswebfolio/src/app/components/Header.jsx
@@ -1,10 +1,15 @@
+import { Meteors } from '@/components/ui/meteors';
+
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
+      <Meteors></Meteors>
         <h1>Tomás Ferreira Webfolio</h1>
       </div>
     </header>
+    
+
   );
 };
 
--- tomaswebfolio/src/app/page.tsx
+++ tomaswebfolio/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Header from './components/Header.jsx';
 import Contact from './components/Contact.jsx';
-import { Meteors } from '../components/ui/meteors.jsx';
+import { Meteors } from '@/components/ui/meteors';
 
 export default function Home() {
   return (
     <>
+   
       <Header />
+      
       <Contact />
-      <Meteors/>
+      
     </>
   );
 }
