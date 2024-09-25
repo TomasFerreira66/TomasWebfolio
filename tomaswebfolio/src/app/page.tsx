54a1afab2daf811104880ec5be6d170269522936
TomasFerreira66 <tomasff2002@gmail.com>
2024-09-25 06:57:56 +0100

    ts

--- tomaswebfolio/src/app/page.tsx
+++ tomaswebfolio/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx';
 import Contact from './components/Contact.jsx';
+import { Meteors } from '../components/ui/meteors.jsx';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Contact />
+      <Meteors/>
     </>
   );
 }
--- tomaswebfolio/tailwind.config.js
+++ tomaswebfolio/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: [ "./src/**/*.{js,ts,jsx,tsx,mdx}",],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "meteor-effect": "meteor 5s linear infinite",
+      },
+      keyframes: {
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
