d6ea1bd4c32032314a0157f63d72cb06bd8d3150
TomasFerreira66 <tomasff2002@gmail.com>
2024-09-27 14:59:33 +0100

    bento grid

--- tomaswebfolio/src/app/page.tsx
+++ tomaswebfolio/src/app/page.tsx
@@ -2,19 +2,25 @@ import Header from './components/Header.jsx';
 import Contact from './components/Contact.jsx';
 import { Meteors } from '@/components/ui/meteors';
 import { ShootingStars } from '@/components/ui/shooting-stars';
-import { StarsBackground } from '@/components/ui/stars-background'
-
-
+import { StarsBackground } from '@/components/ui/stars-background';
+import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid';
+import { BentoGridSecondDemo } from '@/components/ui/bento-grid'; // Ensure this is imported correctly
 
 export default function Home() {
   return (
-    <>
- 
+    <div>
       <Header />
-      <Meteors></Meteors>
+
+
+      {/* Second Bento Grid */}
+      <div className="mt-8">
+        <BentoGridSecondDemo />
+      </div>
+
       <Contact />
       <ShootingStars />
-   <StarsBackground />
-    </>
+      <StarsBackground />
+      <Meteors />
+    </div>
   );
 }
